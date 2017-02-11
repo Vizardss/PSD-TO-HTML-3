f03f025a32b359c5abdf03519d686e47a01c52cf
Kamil Bondaruk <kamil.bondaruk@gmail.com>
2017-02-11 16:49:06 +0100

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,39 @@
 $(document).ready(function(){
-  $('.nav-btn').on('click', function(){
-    $('.main-nav ul').toggleClass('open');
+  // Button #toTop show and hide
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 200) {
+      $('#toTop').fadeIn();
+    } else {
+      $('#toTop').fadeOut();
+    }
   });
+
+  // Navbar fixed
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 600) {
+      $('.header-top').css('background-color', 'rgb(16,22,54)');
+    } else {
+      $('.header-top').css('background-color', 'rgba(16,22,54,.2)');
+    }
+  });
+
+  // Go to top
+  $('#toTop').on('click', function() {
+    $('html,body').animate({scrollTop: 0}, 1500);
+  });
+
+
+// Scroll to section
+  $('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
 });
